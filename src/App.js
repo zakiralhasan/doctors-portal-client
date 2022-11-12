b131ec67cfcbb6f52fe39af282d9787dd8cc8658
zakiralhasan <zakiralhasan.bd@gmail.com>
2022-11-12 18:04:35 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./Routes/Route";
+
+function App() {
+  return (
+    <div className="App">
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Routes/PrivetRout.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PrivetRout = () => {
+  return <div></div>;
+};
+
+export default PrivetRout;
