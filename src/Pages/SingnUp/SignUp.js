3c6ced69206599247db71f8470943e08791a6fdd
zakiralhasan <zakiralhasan.bd@gmail.com>
2022-11-19 00:36:10 +0600

    get user access token from backend

--- src/Pages/SingnUp/SignUp.js
+++ src/Pages/SingnUp/SignUp.js
@@ -66,7 +66,19 @@ const SignUp = () => {
       .then(res => res.json())
       .then(data => {
         console.log(data);
-        navigate('/')
+        getUserToken(email)
+      })
+  }
+
+  //get user access token from backend
+  const getUserToken = (email) => {
+    fetch(`http://localhost:5000/jwt?email=${email}`)
+      .then(res => res.json())
+      .then(data => {
+        if (data.accessToken) {
+          localStorage.setItem('accessToken', data.accessToken)
+          navigate('/')
+        }
       })
   }
 
